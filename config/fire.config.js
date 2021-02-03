ae2de42d9c3dedd1f66802f69ca19db51351f1da
emilie <emi.m.humbert@googlemail.com>
2021-02-03 11:33:31 +0100

    Modify firebase configuraton

--- config/fire.config.js
+++ config/fire.config.js
@@ -1,17 +1,27 @@
-import firebase from "firebase";
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
 
-const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTHDOMAIN,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket: FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-  appId: FIREBASE_APP_I,
-  measurementId: FIREBASE_MEASUREMENT_ID,
-};
+export const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_I,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+}
+
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
-const fire = firebase;
-export default fire;
+const app = firebase.app();
+const auth = firebase.auth();
+const db = firebase.firestore();
+const now = firebase.firestore.Timestamp.now();
+const storage = firebase.storage();
+export { auth, db, now, storage };
+console.log(app.name ? 'Firebase Mode Activated!' : 'Firebase not working :(');
