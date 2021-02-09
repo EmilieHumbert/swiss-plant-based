d35cd340bed4471bbb415e6f29bd67f9d63edc20
emilie <emi.m.humbert@googlemail.com>
2021-02-09 17:05:40 +0100

    Add UI - Button component

--- components/button.js
+++ components/button.js
@@ -1,10 +1,12 @@
-import Spinner from "./icons/spinner";
+import "fontsource-roboto";
+import { Button as MaterialUIButton } from "@material-ui/core";
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 export default function Button({ isLoading, title, children, ...buttonProps }) {
   return (
-    <button {...buttonProps}>
-      {isLoading ? <Spinner width="20" fill="black" /> : title}
+    <MaterialUIButton variant="outlined" {...buttonProps}>
+      {isLoading ? <CircularProgress width="20" /> : title}
       {children}
-    </button>
+    </MaterialUIButton>
   );
 }
