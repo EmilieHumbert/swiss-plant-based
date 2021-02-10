146d4cdc32d2ba41d0870a0ac8d0324df23022de
emilie <emi.m.humbert@googlemail.com>
2021-02-10 18:28:54 +0100

    Add CssBaseline to the app

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
+import "fontsource-roboto";
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 import { AuthProvider } from "../hooks/useAuth";
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <CssBaseline />
       <Component {...pageProps} />
     </AuthProvider>
   );
