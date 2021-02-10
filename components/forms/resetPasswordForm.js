2ba6a56a6b6bb3429752854f099440e4abb07f72
emilie <emi.m.humbert@googlemail.com>
2021-02-10 15:35:59 +0100

    Add UI - Textfield and Button on resetPasswordForm

--- components/forms/resetPasswordForm.js
+++ components/forms/resetPasswordForm.js
@@ -1,38 +1,59 @@
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import { useRouter } from "next/router";
 import { useAuth } from "../../hooks/useAuth";
 
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
 export default function ResetPasswordForm() {
-  const { register, errors, handleSubmit } = useForm();
+  const { control, register, errors, handleSubmit } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
   const auth = useAuth();
   const router = useRouter();
+
   const onSubmit = (data) => {
     auth.sendPasswordResetEmail(data.email);
     router.push("/login");
   };
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div>
-        <label htmlFor="email">Email address</label>
-        <div>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            ref={register({
-              required: "Please enter an email",
-              pattern: {
-                value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                message: "Not a valid email",
-              },
-            })}
-          />
-          {errors.email && <div>{errors.email.message}</div>}
-        </div>
+        <Controller
+          name="email"
+          control={control}
+          rules={{
+            required: "Please enter an email",
+            pattern: {
+              value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+              message: "Not a valid email",
+            },
+          }}
+          render={(props) => (
+            <TextField
+              // id="email"
+              label="Email"
+              type="email"
+              required
+              variant="outlined"
+              value={props.value}
+              onChange={props.onChange}
+              inputRef={props.ref}
+            />
+          )}
+        />
+        {errors.email && <div>{errors.email.message}</div>}
       </div>
       <div>
         <span>
-          <button type="submit">Send reset link</button>
+          <Button type="submit" variant="outlined">
+            Send reset link
+          </Button>
         </span>
       </div>
     </form>
