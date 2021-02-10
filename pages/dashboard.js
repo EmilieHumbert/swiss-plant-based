78308703a5c748b96027d22378c8b843a0f393a2
emilie <emi.m.humbert@googlemail.com>
2021-02-10 18:34:27 +0100

    Add Container to dashboard page

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,14 +1,18 @@
 import { useRequireAuth } from "../hooks/useRequireAuth";
 
+import { Button, Container } from "@material-ui/core";
+
 const DashBoardPage = () => {
   const auth = useRequireAuth();
   if (!auth.user) return null;
   return (
-    <div>
+    <Container maxWidth={"sm"}>
       <h2>{`Welcome ${auth.user.name}!`}</h2>
       <p>{`You are logged in with ${auth.user.email}`}</p>
-      <button onClick={() => auth.signOut()}>Sign out</button>
-    </div>
+      <Button onClick={() => auth.signOut()} variant="outlined">
+        Sign out
+      </Button>
+    </Container>
   );
 };
 
