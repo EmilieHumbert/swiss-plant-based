822059fd7e39c74385403313814fae1fcc33ac01
emilie <emi.m.humbert@googlemail.com>
2021-02-12 17:06:48 +0100

    Add Container and width to profile page

--- pages/profile.js
+++ pages/profile.js
@@ -3,18 +3,21 @@ import Image from "next/image";
 import { useRequireAuth } from "../hooks/useRequireAuth";
 
 import { makeStyles } from "@material-ui/core/styles";
-import { Button, Grid } from "@material-ui/core";
+import { Button, Container, Grid } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    border: "1px solid #dcedc8",
+    backgroundColor: "#f5f5f5",
     flexGrow: 1,
     marginTop: theme.spacing(6),
-  },
-  paper: {
     padding: theme.spacing(2),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
   },
+  image: {
+    borderRadius: "50%",
+  },
+  yourRecipes: { marginTop: theme.spacing(6) },
+  savedRecipes: { marginTop: theme.spacing(6) },
 }));
 
 export default function Profile() {
@@ -22,12 +25,13 @@ export default function Profile() {
   const classes = useStyles();
 
   return auth.loading || !auth.user ? null : (
-    <div className={classes.root}>
-      <Grid container spacing={3}>
+    <Container className={classes.root} maxWidth={"md"} spacing={3}>
+      <Grid container>
         <Grid item xs={6}>
           <Image
             src="/images/profile_picture.jpg"
             alt="Profile picture"
+            className={classes.image}
             width={150}
             height={150}
           />
@@ -35,16 +39,16 @@ export default function Profile() {
         <Grid item xs={6}>
           <h1>{auth.user.name}</h1>
         </Grid>
-        <Grid item xs={12}>
+        <Grid item xs={12} className={classes.yourRecipes}>
           <h2>Your recipes</h2>
           <Button>ADD</Button>
           {/* add list of recipes created by user */}
         </Grid>
-        <Grid item xs={12}>
+        <Grid item xs={12} className={classes.savedRecipes}>
           <h2>Saved recipes</h2>
           {/* add list of recipes saved by user*/}
         </Grid>
       </Grid>
-    </div>
+    </Container>
   );
 }
