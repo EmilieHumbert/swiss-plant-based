3cf4703f2d201c7c447e8f55b506dcc1f05dcd8e
emilie <emi.m.humbert@googlemail.com>
2021-02-12 14:04:34 +0100

    Create Navigation component and add it to the app

---
+++ components/navigation.js
@@ -0,0 +1,60 @@
+import Link from 'next/link'
+
+import { useAuth } from "../hooks/useAuth";
+
+import {
+  AppBar,
+  Button,
+  makeStyles,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  button: {
+    color: "#33691e",
+  },
+  link:{
+    color: "#33691e",
+    textDecoration: 'none',
+  },
+  menuBar: {
+    backgroundColor: "#dcedc8",
+    color: "#33691e",
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function Navigation() {
+  const classes = useStyles();
+  const auth = useAuth();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.menuBar}>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            <Link  href="/">
+              <a className={classes.link} href="#">Home</a>
+            </Link>
+          </Typography>
+          <Button
+            className={classes.button}
+            onClick={() => auth.signOut()}
+            variant="outlined"
+          >
+            Sign out
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import "fontsource-roboto";
-import CssBaseline from '@material-ui/core/CssBaseline';
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 import { AuthProvider } from "../hooks/useAuth";
+import Navigation from "../components/navigation";
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
       <CssBaseline />
+      <Navigation />
       <Component {...pageProps} />
     </AuthProvider>
   );
