db03031294d52e3a1adaecb68a83363e97097baa
emilie <emi.m.humbert@googlemail.com>
2021-02-12 17:53:00 +0100

    Add location item and resize grid title/image

--- pages/profile.js
+++ pages/profile.js
@@ -4,9 +4,9 @@ import Image from "next/image";
 import { useRequireAuth } from "../hooks/useRequireAuth";
 
 import { makeStyles } from "@material-ui/core/styles";
-import { Button, Container, Grid } from "@material-ui/core";
-
+import { Box, Button, Container, Grid } from "@material-ui/core";
 import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -30,7 +30,7 @@ export default function Profile() {
   return auth.loading || !auth.user ? null : (
     <Container className={classes.root} maxWidth={"md"} spacing={3}>
       <Grid container>
-        <Grid item xs={6}>
+        <Grid item xs={3}>
           <Image
             src="/images/profile_picture.jpg"
             alt="Profile picture"
@@ -39,8 +39,12 @@ export default function Profile() {
             height={150}
           />
         </Grid>
-        <Grid item xs={6}>
-          <h1>{auth.user.name}</h1>
+        <Grid item xs={9}>
+          <h1 margin-bottom='0'>{auth.user.name}</h1>
+          <Box display="flex" flexDirection="row" alignItems="center">
+            <LocationOnIcon />
+            <p>the world</p>
+          </Box>
         </Grid>
         <Grid item xs={12} className={classes.yourRecipes}>
           <h2>
