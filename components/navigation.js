64733536fbe509991c36a8ca00ac80b6c05194e9
emilie <emi.m.humbert@googlemail.com>
2021-02-25 18:20:32 +0100

    Add drop down menu in navigation menu

--- components/navigation.js
+++ components/navigation.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Link from "next/link";
 
@@ -9,10 +9,13 @@ import {
   Button,
   Container,
   makeStyles,
+  Menu,
+  MenuItem,
   Toolbar,
   Typography,
 } from "@material-ui/core";
 import PersonIcon from "@material-ui/icons/Person";
+import { withStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -33,12 +36,57 @@ const useStyles = makeStyles((theme) => ({
   menuButton: {
     marginRight: theme.spacing(2),
   },
+  menuItem: {
+    color: "#33691e",
+    textDecoration: "none",
+  },
   title: {
     flexGrow: 1,
   },
 }));
 
+const StyledMenu = withStyles({
+  paper: {
+    border: "1px solid #d3d4d5",
+  },
+})((props) => (
+  <Menu
+    elevation={0}
+    getContentAnchorEl={null}
+    anchorOrigin={{
+      vertical: "bottom",
+      horizontal: "left",
+    }}
+    transformOrigin={{
+      vertical: "top",
+      horizontal: "center",
+    }}
+    {...props}
+  />
+));
+
+const StyledMenuItem = withStyles((theme) => ({
+  root: {
+    "&:focus": {
+      backgroundColor: "#fafafa",
+      "& .MuiListItemIcon-root, & .MuiListItemText-primary": {
+        color: "theme.palette.common.white",
+      },
+    },
+  },
+}))(MenuItem);
+
 export default function Navigation() {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   const classes = useStyles();
   const auth = useAuth();
 
@@ -53,12 +101,29 @@ export default function Navigation() {
               </a>
             </Link>
           </Typography>
-          <Button>
-            <Link href="/settings">
-              <PersonIcon />
-            </Link>
+          <Button aria-controls="customized-menu" onClick={handleClick}>
+            <PersonIcon />
           </Button>
+          <StyledMenu
+            id="customized-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <StyledMenuItem>
+              <Link href="/profile">
+                <a className={classes.menuItem}>Profile</a>
+              </Link>
+            </StyledMenuItem>
+            <StyledMenuItem>
+              <Link href="/settings">
+                <a className={classes.menuItem}>Settings</a>
+              </Link>
+            </StyledMenuItem>
+          </StyledMenu>
           <Button
+            aria-controls="customized-menu"
             className={classes.button}
             onClick={() => auth.signOut()}
             variant="outlined"
