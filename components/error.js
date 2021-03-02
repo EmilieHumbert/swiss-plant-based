818a7a5aa102186ddb8d34e198f3ae6bda1303f5
emilie <emi.m.humbert@googlemail.com>
2021-03-02 16:25:01 +0100

    Improve UI for error message

--- components/error.js
+++ components/error.js
@@ -4,8 +4,9 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
   errorMessage: {
     backgroundColor: "#F8D7DA",
-    borderRadius: "15px",
-    color: "A14D55",
+    borderRadius: "4px",
+    border:'#D86363 solid 1px',
+    color: "#A14D55",
     marginTop: "16px",
     padding: "16px",
   },
