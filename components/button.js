98e149dc4b21553fc7076e5bdd42736a41a9252a
emilie <emi.m.humbert@googlemail.com>
2021-03-04 15:51:58 +0100

    Add styles on the button component

--- components/button.js
+++ components/button.js
@@ -2,10 +2,24 @@ import React from "react";
 
 import { Button as MaterialUIButton } from "@material-ui/core";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  btn: {
+    color: "#33691e",
+    margin: "16px 16px 16px 0",
+  },
+}));
 
 export default function Button({ isLoading, title, children, ...buttonProps }) {
+  const classes = useStyles();
   return (
-    <MaterialUIButton variant="outlined" {...buttonProps}>
+    <MaterialUIButton
+      variant="outlined"
+      {...buttonProps}
+      className={classes.btn}
+    >
+      {console.log("button", { buttonProps })}
       {isLoading ? <CircularProgress width="20" /> : title}
       {children}
     </MaterialUIButton>
